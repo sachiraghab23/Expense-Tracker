aab161c6a9f8273a4ba9589bf396c2c303a5bc4f
sachiraghab <sachiraghab@gmail.com>
2023-09-25 02:36:46 +0530

    Header and Footer added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import "./App.css";
+import {Routes,Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="container-fluid">
-      <h1 className="text-center">hello</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </>
   );
 }
 
